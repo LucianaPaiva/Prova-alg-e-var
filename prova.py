1f0046bed9ab07f14b4539ec4769b9d65c5e6e76
Luciana Paiva <luciana@vutus.com>
2023-10-08 12:34:22 -0300

    Initial commit

---
+++ prova.py
@@ -0,0 +1,43 @@
+# #Exemplo de variavel do tipo de dado String (str)
+# nome = "Luciana Paiva"
+# print (type(nome))
+
+# nome = "Luciana Paiva"
+# print(nome)
+
+# #Exemplo de variavel do tipo de dado inteiro (int)
+# idade = 42
+# print (type(idade))
+
+# idade = 42
+# print(idade)
+
+# #Exemplo de variavel do tipo de dado decimal (float)
+# peso = 69.2
+# print(type(peso))
+
+# peso = 69.2
+# print(peso)
+
+# #Exemplo de variavel do tipo de dado (bool)
+# brasileira = True
+# print(type(brasileira))
+
+# brasileira = True
+# print(brasileira)
+
+
+# nome é uma variável que vai receber uma string (str)
+nome = input("Digite seu nome: ")
+
+# idade é uma variável que vai receber uma string (int)
+idade = input("Digite sua idade: ")
+
+# brasileira é uma variável que vai receber uma string (bool)
+brasileiro = input("Digite se é brasileiro: ")
+
+# peso é uma variável que vai receber uma string (float)
+peso = input("Digite seu peso: ")
+
+print("---------------------------------")
+print(f'Ola {nome}, você tem {idade}anos. Você é brasileira? {brasileiro}. E seu peso é {peso} kg')
